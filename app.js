6fee35703ee1cffe02ed56fe8e193e62d2967301
Ariel Pardo <ariel.marcelo.pardo@accenture.com>
2025-01-27 15:57:57 -0300

    Finished section 94: usando parciales con HBS'

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
 const express = require('express');
+const hbs = require('hbs');
+
 const app = express();
 const port = 8080;
 
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static('public')); //esto es un middleware
 
 
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('home', {
+	nombre: 'Ariel Pardo',
+	titulo: 'Curso de NodeJs'
+    }); 
 });
 
 app.get('/generic', (req, res) => {
-    res.sendFile(__dirname + '/public/generic.html');
+    res.render('generic', {
+	nombre: 'Ariel Pardo',
+	titulo: 'Curso de NodeJs'
+    });
 });
 
 app.get('/elements', (req, res) => {
-    res.sendFile(__dirname + '/public/elements.html');
+    res.render('elements', {
+	nombre: 'Ariel Pardo',
+	titulo: 'Curso de NodeJs'
+    });
 });
 
 app.get('*', (req, res) => {
